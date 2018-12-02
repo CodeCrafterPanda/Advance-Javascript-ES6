eb6a5ecade8f86a417e7f97a7e9b58190abaff29
Sushilkumar Shinde <sushilkumarshinde06@gmail.com>
2018-12-02 18:28:46 +0530

    Advance Javascript ES6

---
+++ Github-Finder/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '0450c623bcbecd38ec3a';
+    this.client_secret = '76a9103d9dc7d0c6101c42e74c9a97321e673919';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
